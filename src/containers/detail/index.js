a3cfff40129cf6a4e5a27d5d12845fd23db15c31
Hoang Nguyen <hoang.nguyen2@niteco.se>
2020-09-21 14:56:47 +0700

    swiper for detail

--- src/containers/detail/index.js
+++ src/containers/detail/index.js
@@ -1,39 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../../styles/detail.scss';
 import { useParams } from "react-router-dom";
 import {useSelector} from 'react-redux';
 import {getData} from "../../store/selectors";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
+import 'swiper/swiper.scss';
 
 function DetailContainer() {
-
 
   let { id } = useParams();
   let data = useSelector(state => getData(state, id));
 
   let technicals = data.data[2].description.split(',');
 
+  SwiperCore.use([Autoplay]);
+
   return (
     <div className="detail">
       <img className="detail__clientlogo" src={data.clientLogo}/>
       <img className="detail__nitecologo" src={data.nitecoLogo}/>
-      <div
+      <Swiper
+        className="detail__swiper"
         spaceBetween={0}
+        autoplay={{
+          delay: 5000,
+        }}
         slidesPerView={1}
         direction="vertical"
       >
-        <div className="detail__slide">
+        <SwiperSlide>
           <img className="detail__brief-image" src={data.data[0].image} />
           <p className="detail__brief-name">THE BRIEF</p>
           <p className="detail__brief-description " dangerouslySetInnerHTML={{__html: data.data[0].description}} />
-        </div>
+        </SwiperSlide>
 
-        <div className="detail__slide" >
+        <SwiperSlide>
           <p className="detail__solution-name">THE SOLUTION</p>
           <p className="detail__solution-description" dangerouslySetInnerHTML={{__html: data.data[1].description}} />
           <img className="detail__solution-image" src={data.data[1].image} />
-        </div>
+        </SwiperSlide>
 
-        <div  className="detail__slide detail__technical">
+        <SwiperSlide className="detail__technical">
           <div className="detail__technical__image-container">
             <img className="detail__technical-image" src={data.data[2].image}/>
           </div>
@@ -43,8 +51,8 @@ function DetailContainer() {
               {technicals.map(e => <li className="detail__technical-item" dangerouslySetInnerHTML={{__html: e}} />)}
             </ul>
           </div>
-        </div>
-      </div>
+        </SwiperSlide>
+      </Swiper>
 
     </div>
   )
